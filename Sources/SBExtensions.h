1c2ee68344eb2c979d05010aad337c6b5cde8a44
Max <sibo_jian_29903549@163.com>
2023-11-02 09:08:42 +0800

    feat: Initial SBExtensions project

---
+++ Sources/SBExtensions.h
@@ -0,0 +1,18 @@
+//
+//  SBExtensions.h
+//  SBExtensions
+//
+//  Created by JONO-Jsb on 2023/11/2.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SBExtensions.
+FOUNDATION_EXPORT double SBExtensionsVersionNumber;
+
+//! Project version string for SBExtensions.
+FOUNDATION_EXPORT const unsigned char SBExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBExtensions/PublicHeader.h>
+
+
