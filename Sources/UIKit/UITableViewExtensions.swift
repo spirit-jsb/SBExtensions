5329712532b8faedef20e13fe230b48ab152e896
Max <sibo_jian_29903549@163.com>
2023-11-10 16:28:01 +0800

    feat: Add UITableView extensions functionality
    * lastRowIndexPath property
    * lastSection property
    * numberOfRows property
    * removeTableHeaderView function
    * removeTableFooterView function
    * indexPathOfLastRow(inSection:) function
    * safeScrollToRow(at:at:animated:) function

--- Sources/UIKit/UITableViewExtensions.swift
+++ Sources/UIKit/UITableViewExtensions.swift
@@ -10,6 +10,28 @@
 import UIKit
 
 public extension UITableView {
+    var lastRowIndexPath: IndexPath? {
+        return self.indexPathOfLastRow(inSection: self.lastSection)
+    }
+
+    var lastSection: Int {
+        return self.numberOfSections > 0 ? self.numberOfSections - 1 : 0
+    }
+
+    var numberOfRows: Int {
+        var section = 0
+        var rowsCount = 0
+
+        while section < self.numberOfSections {
+            rowsCount += self.numberOfRows(inSection: section)
+            section += 1
+        }
+
+        return rowsCount
+    }
+}
+
+public extension UITableView {
     func registerCell<T: UITableViewCell>(_ cellType: T.Type) {
         self.register(cellType, forCellReuseIdentifier: cellType.identifier)
     }
@@ -45,6 +67,32 @@ public extension UITableView {
             fatalError("Could not dequeue a view with identifier \(viewType.identifier) - must register a nib or a class for the identifier or connect a prototype cell in a storyboard")
         }
         return headerFooterView
+    }
+
+    func removeTableHeaderView() {
+        self.tableHeaderView = nil
+    }
+
+    func removeTableFooterView() {
+        self.tableFooterView = nil
+    }
+
+    func indexPathOfLastRow(inSection section: Int) -> IndexPath? {
+        guard section >= 0, section < self.numberOfSections else {
+            return nil
+        }
+
+        let rowsCount = self.numberOfRows(inSection: section)
+
+        return IndexPath(item: rowsCount > 0 ? rowsCount - 1 : 0, section: section)
+    }
+
+    func safeScrollToRow(at indexPath: IndexPath, at scrollPosition: UITableView.ScrollPosition, animated: Bool) {
+        guard indexPath.section >= 0, indexPath.section < self.numberOfSections, indexPath.row >= 0, indexPath.row < self.numberOfRows(inSection: indexPath.section) else {
+            return
+        }
+
+        self.scrollToRow(at: indexPath, at: scrollPosition, animated: animated)
     }
 }
 
