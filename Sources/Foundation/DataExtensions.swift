bccac09c2767a0c0362290271b9a3987dbacbce0
Max <sibo_jian_29903549@163.com>
2023-11-13 15:37:44 +0800

    feat: Add Data extensions functionality
    * bytes property
    * jsonObject property
    * jsonString property
    * hexString property
    * base64String property
    * string(encoding:) function

---
+++ Sources/Foundation/DataExtensions.swift
@@ -0,0 +1,46 @@
+//
+//  DataExtensions.swift
+//  SBExtensions
+//
+//  Created by JONO-Jsb on 2023/11/13.
+//
+
+#if canImport(Foundation)
+
+import Foundation
+
+public extension Data {
+    var bytes: [UInt8] {
+        // http://stackoverflow.com/questions/38097710/swift-3-changes-for-getbytes-method
+        return [UInt8](self)
+    }
+
+    var jsonObject: Any? {
+        return try? JSONSerialization.jsonObject(with: self, options: [])
+    }
+
+    var jsonString: String? {
+        guard let jsonObject = try? JSONSerialization.jsonObject(with: self, options: []), let jsonData = try? JSONSerialization.data(withJSONObject: jsonObject, options: [.prettyPrinted]) else {
+            return nil
+        }
+
+        return String(data: jsonData, encoding: .utf8)
+    }
+
+    var hexString: String {
+        // https://stackoverflow.com/questions/39075043/how-to-convert-data-to-hex-string-in-swift
+        return self.map { String(format: "%02x", $0) }.joined()
+    }
+
+    var base64String: String {
+        return self.base64EncodedString()
+    }
+}
+
+public extension Data {
+    func string(encoding: String.Encoding) -> String? {
+        return String(data: self, encoding: encoding)
+    }
+}
+
+#endif
