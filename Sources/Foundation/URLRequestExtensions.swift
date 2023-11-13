e720797d7dc22f221f531c0f39d79d14d1a2315f
Max <sibo_jian_29903549@163.com>
2023-11-13 15:34:24 +0800

    feat: Add URLRequest extensions functionality
    * cURL property

---
+++ Sources/Foundation/URLRequestExtensions.swift
@@ -0,0 +1,45 @@
+//
+//  URLRequestExtensions.swift
+//  SBExtensions
+//
+//  Created by JONO-Jsb on 2023/11/13.
+//
+
+#if canImport(Foundation)
+
+import Foundation
+
+public extension URLRequest {
+    var cURL: String {
+        guard let url = self.url, let method = self.httpMethod else {
+            return "$ curl command could not be created"
+        }
+
+        var components = ["$ curl -v"]
+
+        components.append("-X \(method)")
+
+        if let httpHeaders = self.allHTTPHeaderFields {
+            for httpHeader in httpHeaders where httpHeader.key != "Cookie" {
+                let escapedValue = httpHeader.value.replacingOccurrences(of: "\"", with: "\\\"")
+
+                components.append("-H \"\(httpHeader.key): \(escapedValue)\"")
+            }
+        }
+
+        if let httpBodyData = self.httpBody {
+            let httpBody = String(decoding: httpBodyData, as: UTF8.self)
+
+            var escapedBody = httpBody.replacingOccurrences(of: "\\\"", with: "\\\\\"")
+            escapedBody = escapedBody.replacingOccurrences(of: "\"", with: "\\\"")
+
+            components.append("-d \"\(escapedBody)\"")
+        }
+
+        components.append("\"\(url.absoluteString)\"")
+
+        return components.joined(separator: " \\\n\t")
+    }
+}
+
+#endif
