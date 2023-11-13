eca164713eb03dbcfb61f99c2aeeca62eda961eb
Max <sibo_jian_29903549@163.com>
2023-11-13 08:36:59 +0800

    feat: Add Cornered functionality
    * allCorners property
    * topLeft property
    * topRight property
    * bottomLeft property
    * bottomRight property
    * maskedCorners property
    * roundingCorners property

---
+++ Sources/Cornered.swift
@@ -0,0 +1,73 @@
+//
+//  Cornered.swift
+//  SBExtensions
+//
+//  Created by JONO-Jsb on 2023/11/13.
+//
+
+#if canImport(QuartzCore) && canImport(UIKit)
+
+import QuartzCore
+import UIKit
+
+public struct Corner: OptionSet {
+    public static let allCorners = Corner(rawValue: ~0)
+    public static let topLeft = Corner(rawValue: 1 << 0)
+    public static let topRight = Corner(rawValue: 1 << 1)
+    public static let bottomLeft = Corner(rawValue: 1 << 2)
+    public static let bottomRight = Corner(rawValue: 1 << 3)
+
+    var maskedCorners: CACornerMask {
+        var maskedCorners = CACornerMask()
+
+        if self.contains(.allCorners) {
+            maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner, .layerMaxXMaxYCorner, .layerMinXMaxYCorner]
+        } else {
+            if self.contains(.topLeft) {
+                maskedCorners.insert(.layerMinXMinYCorner)
+            }
+            if self.contains(.topRight) {
+                maskedCorners.insert(.layerMaxXMinYCorner)
+            }
+            if self.contains(.bottomRight) {
+                maskedCorners.insert(.layerMaxXMaxYCorner)
+            }
+            if self.contains(.bottomLeft) {
+                maskedCorners.insert(.layerMinXMaxYCorner)
+            }
+        }
+
+        return maskedCorners
+    }
+
+    var roundingCorners: UIRectCorner {
+        var roundingCorners = UIRectCorner()
+
+        if self.contains(.allCorners) {
+            roundingCorners = [.allCorners]
+        } else {
+            if self.contains(.topLeft) {
+                roundingCorners.insert(.topLeft)
+            }
+            if self.contains(.topRight) {
+                roundingCorners.insert(.topRight)
+            }
+            if self.contains(.bottomRight) {
+                roundingCorners.insert(.bottomRight)
+            }
+            if self.contains(.bottomLeft) {
+                roundingCorners.insert(.bottomLeft)
+            }
+        }
+
+        return roundingCorners
+    }
+
+    public var rawValue: UInt
+
+    public init(rawValue: UInt) {
+        self.rawValue = rawValue
+    }
+}
+
+#endif
