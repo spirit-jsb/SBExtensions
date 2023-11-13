8c3f3ace4baaa0be27938588c6ab621da3dd3b78
Max <sibo_jian_29903549@163.com>
2023-11-13 09:55:15 +0800

    feat: Add CGRect extensions functionality
    * center property
    * resizing(to:anchor:) function
    * init(center:size:) function

---
+++ Sources/CoreGraphics/CGRectExtensions.swift
@@ -0,0 +1,36 @@
+//
+//  CGRectExtensions.swift
+//  SBExtensions
+//
+//  Created by JONO-Jsb on 2023/11/13.
+//
+
+#if canImport(CoreGraphics)
+
+import CoreGraphics
+
+public extension CGRect {
+    var center: CGPoint {
+        return CGPoint(x: self.midX, y: self.midY)
+    }
+}
+
+public extension CGRect {
+    func resizing(to size: CGSize, anchor: CGPoint = CGPoint(x: 0.5, y: 0.5)) -> CGRect {
+        let sizeDelta = CGSize(width: size.width - self.width, height: size.height - self.height)
+
+        let origin = CGPoint(x: self.minX - sizeDelta.width * anchor.x, y: self.minY - sizeDelta.height * anchor.y)
+
+        return CGRect(origin: origin, size: sizeDelta)
+    }
+}
+
+public extension CGRect {
+    init(center: CGPoint, size: CGSize) {
+        let origin = CGPoint(x: center.x - size.width / 2.0, y: center.y - size.height / 2.0)
+
+        self.init(origin: origin, size: size)
+    }
+}
+
+#endif
