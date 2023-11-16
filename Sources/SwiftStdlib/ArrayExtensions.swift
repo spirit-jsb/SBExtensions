ca42a717329e2826577bcda3260e8d530dde9889
Max <sibo_jian_29903549@163.com>
2023-11-16 14:15:13 +0800

    feat: Add Array extensions functionality
    * prepend(_:) function
    * safeSwapAt(_:_:) function
    * sort(like:keyPath:) function
    * sorted(like:keyPath:) function
    * removeDuplicates(_:) function
    * removedDuplicates(_:) function
    * removeDuplicates function
    * removedDuplicates function
    * removeAll(_:) function
    * removeAll(_:) function
    * init(count:initializer:) function

---
+++ Sources/SwiftStdlib/ArrayExtensions.swift
@@ -0,0 +1,107 @@
+//
+//  ArrayExtensions.swift
+//  SBExtensions
+//
+//  Created by JONO-Jsb on 2023/11/16.
+//
+
+public extension Array {
+    mutating func prepend(_ newElement: Element) {
+        self.insert(newElement, at: 0)
+    }
+
+    mutating func safeSwapAt(_ from: Int, _ to: Int) {
+        guard from != to else {
+            return
+        }
+        guard self.startIndex ..< self.endIndex ~= from else {
+            return
+        }
+        guard self.startIndex ..< self.endIndex ~= to else {
+            return
+        }
+
+        self.swapAt(from, to)
+    }
+
+    mutating func sort<T: Equatable>(like orderArray: [T], keyPath: KeyPath<Element, T>) {
+        self = self.sorted(like: orderArray, keyPath: keyPath)
+    }
+
+    func sorted<T: Equatable>(like orderArray: [T], keyPath: KeyPath<Element, T>) -> [Element] {
+        return self.lazy.sorted { lhs, rhs in
+            let lhsValue = lhs[keyPath: keyPath]
+            let rhsValue = rhs[keyPath: keyPath]
+
+            // swiftformat:disable:next acronyms
+            if let lhsIdx = orderArray.firstIndex(of: lhsValue), let rhsIdx = orderArray.firstIndex(of: rhsValue) {
+                // swiftformat:disable:next acronyms
+                return lhsIdx < rhsIdx
+            } else if orderArray.contains(lhsValue) {
+                return true
+            } else if orderArray.contains(rhsValue) {
+                return false
+            } else {
+                return false
+            }
+        }
+    }
+
+    mutating func removeDuplicates<T: Equatable>(_ keyPath: KeyPath<Element, T>) {
+        self = self.removedDuplicates(keyPath)
+    }
+
+    func removedDuplicates<T: Equatable>(_ keyPath: KeyPath<Element, T>) -> [Element] {
+        return self.lazy.enumerated()
+            .filter { idx, element in
+                self.firstIndex { $0[keyPath: keyPath] == element[keyPath: keyPath] } == idx
+            }
+            .map { _, element in
+                element
+            }
+    }
+
+    mutating func removeDuplicates() where Element: Equatable {
+        self = self.removedDuplicates()
+    }
+
+    func removedDuplicates() -> [Element] where Element: Equatable {
+        return self.lazy.enumerated()
+            .filter { idx, element in
+                self.firstIndex { $0 == element } == idx
+            }
+            .map { _, element in
+                element
+            }
+    }
+
+    @discardableResult
+    mutating func removeAll(_ element: Element) -> [Element] where Element: Equatable {
+        self.removeAll(where: { $0 == element })
+
+        return self
+    }
+
+    @discardableResult
+    mutating func removeAll(_ elements: [Element]) -> [Element] where Element: Equatable {
+        guard !elements.isEmpty else {
+            return self
+        }
+
+        self.removeAll(where: { elements.contains($0) })
+
+        return self
+    }
+}
+
+public extension Array {
+    init(count: Int, initializer: (Int) throws -> Element) rethrows {
+        try self.init(unsafeUninitializedCapacity: count) { buffer, initializedCount in
+            for idx in 0 ..< initializedCount {
+                try buffer.baseAddress?.advanced(by: idx).initialize(to: initializer(idx))
+            }
+
+            initializedCount = count
+        }
+    }
+}
