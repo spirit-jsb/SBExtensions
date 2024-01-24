38d42053d63d3dc99c284c8e301cb665ee9fd6a6
Max <sibo_jian_29903549@163.com>
2024-01-24 17:45:29 +0800

    test: Add UIButton extensions tests and related classes to project

---
+++ Tests/Utils/XCTestExtensions.swift
@@ -0,0 +1,44 @@
+//
+//  XCTestExtensions.swift
+//  SBExtensionsTests
+//
+//  Created by 菅思博 on 2024/1/24.
+//
+
+#if canImport(XCTest)
+
+import XCTest
+
+#if canImport(UIKit)
+
+import UIKit
+
+#endif
+
+#if canImport(UIKit)
+
+func XCTAssertEqual(_ expression1: @autoclosure () throws -> UIColor, _ expression2: @autoclosure () throws -> UIColor, accuracy: CGFloat, _ message: @autoclosure () -> String = "", file: StaticString = #filePath, line: UInt = #line) {
+    var color1: UIColor!
+    XCTAssertNoThrow(color1 = try expression1(), message(), file: file, line: line)
+    
+    var color2: UIColor!
+    XCTAssertNoThrow(color2 = try expression2(), message(), file: file, line: line)
+    
+    var red1: CGFloat = 0.0, green1: CGFloat = 0.0, blue1: CGFloat = 0.0, alpha1: CGFloat = 0.0
+    var red2: CGFloat = 0.0, green2: CGFloat = 0.0, blue2: CGFloat = 0.0, alpha2: CGFloat = 0.0
+    
+    color1.getRed(&red1, green: &green1, blue: &blue1, alpha: &alpha1)
+    color2.getRed(&red2, green: &green2, blue: &blue2, alpha: &alpha2)
+    
+    print(">>> red1 \(red1)")
+    print(">>> red2 \(red2)")
+    
+    XCTAssertEqual(red1, red2, accuracy: accuracy, message(), file: file, line: line)
+    XCTAssertEqual(green1, green2, accuracy: accuracy, message(), file: file, line: line)
+    XCTAssertEqual(blue1, blue2, accuracy: accuracy, message(), file: file, line: line)
+    XCTAssertEqual(alpha1, alpha2, accuracy: accuracy, message(), file: file, line: line)
+}
+
+#endif
+
+#endif
