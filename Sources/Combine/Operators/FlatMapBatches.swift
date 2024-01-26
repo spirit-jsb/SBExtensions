1e12c5953d0a17ff58f4a5b258e3f4f27b44349f
Max <sibo_jian_29903549@163.com>
2024-01-26 19:51:24 +0800

    feat: Add FlatMapBatches functionality

---
+++ Sources/Combine/Operators/FlatMapBatches.swift
@@ -0,0 +1,27 @@
+//
+//  FlatMapBatches.swift
+//  SBExtensions
+//
+//  Created by 菅思博 on 2024/1/26.
+//
+
+#if canImport(Combine)
+
+import Combine
+
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+public extension Collection where Element: Publisher {
+    func flatMapBatches(of size: Int) -> AnyPublisher<[Element.Output], Element.Failure> {
+        precondition(size > 0, "Batch sizes must be positive")
+        
+        let indexBreaks = sequence(first: startIndex, next: { $0 == endIndex ? nil : index($0, offsetBy: size, limitedBy: endIndex) ?? endIndex })
+        
+        return Swift.zip(indexBreaks, indexBreaks.dropFirst())
+            .publisher
+            .setFailureType(to: Element.Failure.self)
+            .flatMap(maxPublishers: .max(1)) { self[$0..<$1].zip() }
+            .eraseToAnyPublisher()
+    }
+}
+
+#endif
