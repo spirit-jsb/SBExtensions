52f0602a7b9b8b65f10203282df78c7eab3120b7
Max <sibo_jian_29903549@163.com>
2024-01-30 14:28:02 +0800

    feat: Add RetryWhen functionality

--- Sources/Combine/Operators/RetryWhen.swift
+++ Sources/Combine/Operators/RetryWhen.swift
@@ -11,8 +11,8 @@ import Combine
 
 @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 public extension Publisher {
-    func retryWhen<TriggerPublisher>(_ notificationHandler: @escaping (AnyPublisher<Failure, Never>) -> TriggerPublisher) -> Publishers.RetryWhen<Self, TriggerPublisher, Output, Failure> where TriggerPublisher: Publisher {
-        .init(upstream: self, notificationHandler: notificationHandler)
+    func retryWhen<TriggerPublisher>(_ failureHandler: @escaping (AnyPublisher<Failure, Never>) -> TriggerPublisher) -> Publishers.RetryWhen<Self, TriggerPublisher, Output, Failure> where TriggerPublisher: Publisher {
+        .init(upstream: self, failureHandler: failureHandler)
     }
 }
 
@@ -20,22 +20,22 @@ public extension Publisher {
 public extension Publishers {
     class RetryWhen<Upstream, TriggerPublisher, Output, Failure>: Publisher where Upstream: Publisher, TriggerPublisher: Publisher, Upstream.Output == Output, Upstream.Failure == Failure {
         private let upstream: Upstream
-        private let notificationHandler: (AnyPublisher<Upstream.Failure, Never>) -> TriggerPublisher
+        private let failureHandler: (AnyPublisher<Upstream.Failure, Never>) -> TriggerPublisher
 
-        init(upstream: Upstream, notificationHandler: @escaping (AnyPublisher<Upstream.Failure, Never>) -> TriggerPublisher) {
+        init(upstream: Upstream, failureHandler: @escaping (AnyPublisher<Upstream.Failure, Never>) -> TriggerPublisher) {
             self.upstream = upstream
-            self.notificationHandler = notificationHandler
+            self.failureHandler = failureHandler
         }
 
         public func receive<S>(subscriber: S) where S: Subscriber, S.Input == Output, S.Failure == Failure {
-            subscriber.receive(subscription: Subscription(upstream: self.upstream, downstream: subscriber, errorTrigger: self.errorTrigger))
+            subscriber.receive(subscription: Subscription(upstream: self.upstream, downstream: subscriber, failureHandler: self.failureHandler))
         }
     }
 }
 
 @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 extension Publishers.RetryWhen {
-    class Subscription<Downstream>: Combine.Subscription where Downstream: Subscriber, Downstream.Input == Output, Downstream.Failure == Failure {
+    class Subscription<Downstream>: Combine.Subscription where Downstream: Subscriber, Downstream.Input == Upstream.Output, Downstream.Failure == Upstream.Failure {
         var description: String {
             return "RetryWhen.Subscription<\(Output.self), \(Failure.self)>"
         }
@@ -43,12 +43,12 @@ extension Publishers.RetryWhen {
         private let upstream: Upstream
         private let downstream: Downstream
 
-        private let errorSubject = PassthroughSubject<Failure, Never>()
+        private let errorSubject = PassthroughSubject<Upstream.Failure, Never>()
 
         private var sink: Sink<Upstream, Downstream>?
         private var cancellable: AnyCancellable?
 
-        init(upstream: Upstream, downstream: Downstream, notificationHandler: @escaping (AnyPublisher<Failure, Never>) -> TriggerPublisher) {
+        init(upstream: Upstream, downstream: Downstream, failureHandler: @escaping (AnyPublisher<Upstream.Failure, Never>) -> TriggerPublisher) {
             self.upstream = upstream
             self.downstream = downstream
 
@@ -60,13 +60,13 @@ extension Publishers.RetryWhen {
                 return nil
             })
 
-            self.cancellable = notificationHandler(self.errorSubject.eraseToAnyPublisher())
+            self.cancellable = failureHandler(self.errorSubject.eraseToAnyPublisher())
                 .sink(receiveCompletion: { [sink] completion in
                     switch completion {
                         case .finished:
                             sink?.buffer.complete(completion: .finished)
-                        case let .failure(error):
-                            if let error = error as? Failure {
+                        case .failure(let error):
+                            if let error = error as? Downstream.Failure {
                                 sink?.buffer.complete(completion: .failure(error))
                             }
                     }
